bf21dc1a574dccd8d57fa9e411fcd6053c192e07
Ivor-tan <1336957191@qq.com>
2020-10-09 22:41:57 +0800

    1.1 单一职责原则

--- app/src/main/java/com/ivor/androidstudy_design_pattern_2020_10_09/MainActivity.java
+++ app/src/main/java/com/ivor/androidstudy_design_pattern_2020_10_09/MainActivity.java
@@ -3,12 +3,23 @@ package com.ivor.androidstudy_design_pattern_2020_10_09;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.ivor.androidstudy_design_pattern_2020_10_09.OneChapter.two.ImageLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String imageUrl = "https://c-ssl.duitang.com/uploads/item/201512/22/20151222223118_tnvcX.thumb.1000_0.jpeg";
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        ImageLoader imageLoader_ = new ImageLoader();
+        imageLoader_.displayImage(imageUrl, imageView);
     }
 }
---
+++ app/src/main/java/com/ivor/androidstudy_design_pattern_2020_10_09/OneChapter/two/ImageLoader.java
@@ -0,0 +1,86 @@
+package com.ivor.androidstudy_design_pattern_2020_10_09.OneChapter.two;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 图片加载类
+ */
+
+public class ImageLoader {
+
+    String TAG = getClass().getName();
+
+    ImageCache mImageCache = new ImageCache();
+
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    Handler mUIHandler = new Handler(Looper.getMainLooper());
+
+    public ImageLoader() {
+        initImageLoader();
+    }
+
+    private void initImageLoader() {
+
+    }
+
+    public void displayImage(final String url, final ImageView imageView) {
+        imageView.setTag(url);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(url);
+                if (bitmap == null) {
+                    return;
+                }
+                if (imageView.getTag().equals(url)) {
+                    updateImageView(imageView, bitmap);
+                }
+                mImageCache.put(url, bitmap);
+            }
+        });
+    }
+
+
+    private void updateImageView(final ImageView imageView, final Bitmap bitmap) {
+        mUIHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+    }
+
+    private Bitmap downloadImage(String imageUrl) {
+        Bitmap bitmap = null;
+
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+            connection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (bitmap != null) {
+            Log.d(TAG, "bitmap: size======>" + bitmap.getRowBytes() * bitmap.getHeight() / 1024);
+        }
+        return bitmap;
+    }
+
+}
